678ac271b6d18a36ce814f9d3dcbcbbb16a2bafd
namangoyal497 <Namangoyal497@gmail.com>
2024-05-13 01:03:01 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-
+import path from 'path'
 import express from "express"
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser";
@@ -12,6 +12,7 @@ import {app, server} from "./socket/socket.js"
 import { Socket } from "socket.io";
 const PORT=process.env.PORT || 5000;
 
+const __dirname=path.resolve();
 dotenv.config()
 // app.get("/",(req,res)=>{
 //     res.send("Hello world")
@@ -23,6 +24,11 @@ app.use("/api/auth",authRoutes)
 app.use("/api/messages",messageRoutes)
 app.use("/api/users",userRoutes)
 
+app.use(express.static(path.join(__dirname,"/frontend/dist")))
+
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"frontend","dist","index.html"))
+})
 server.listen(PORT,()=>{
     connectTomongodb();
     console.log(`Server running on the port ${PORT}`)
