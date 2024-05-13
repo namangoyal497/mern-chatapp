b8959ae9742aa2341ee8aebbdb54ae1d22b7b925
namangoyal497 <Namangoyal497@gmail.com>
2024-05-13 10:15:53 +0530

    socket link updated

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -1,44 +1,83 @@
-import { createContext , useState,useEffect,useContext} from "react";
-import {useAuthContext} from './AuthContext';
-import io from "socket.io-client"
-export const SocketContext=createContext();
+// import { createContext , useState,useEffect,useContext} from "react";
+// import {useAuthContext} from './AuthContext';
+// import io from "socket.io-client"
+// export const SocketContext=createContext();
 
-export const useSocketContext = () => {
-	return useContext(SocketContext);
-};
+// export const useSocketContext = () => {
+// 	return useContext(SocketContext);
+// };
 
-export const SocketContextProvider=({children})=>{
+// export const SocketContextProvider=({children})=>{
     
-    const [socket,setSocket]=useState(null);
-    const [onlineUsers,setOnlineUsers]=useState([]);
-    const {authUser}=useAuthContext();
-    useEffect(()=>{
-        if(authUser){
-            const socket=io("http://localhost:5000",{
-                query:{
-                    userId : authUser._id,
-                }
-            }
-            );
+//     const [socket,setSocket]=useState(null);
+//     const [onlineUsers,setOnlineUsers]=useState([]);
+//     const {authUser}=useAuthContext();
+//     useEffect(()=>{
+//         if(authUser){
+//             const socket=io("http://localhost:5000",{
+//                 query:{
+//                     userId : authUser._id,
+//                 }
+//             }
+//             );
 
-            setSocket(socket);
+//             setSocket(socket);
 
-            socket.on("getOnlineUsers",(users)=>{
-                setOnlineUsers(users);
-            })
-            return ()=>socket.close();
-        }
-        else{
-            if(socket){
-                socket.close();
-                setSocket(null);
-            }
-        }
-    },[authUser]);
+//             socket.on("getOnlineUsers",(users)=>{
+//                 setOnlineUsers(users);
+//             })
+//             return ()=>socket.close();
+//         }
+//         else{
+//             if(socket){
+//                 socket.close();
+//                 setSocket(null);
+//             }
+//         }
+//     },[authUser]);
 
-    return (
-        <SocketContext.Provider value={{socket,onlineUsers}}>
-            {children}
-        </SocketContext.Provider>
-    )
-}
+//     return (
+//         <SocketContext.Provider value={{socket,onlineUsers}}>
+//             {children}
+//         </SocketContext.Provider>
+//     )
+// }
+import { createContext, useEffect, useState, useContext } from "react";
+import { useAuthContext } from "./AuthContext";
+import io from "socket.io-client";
+
+export const SocketContext = createContext();
+export const useSocketContext = () => {
+  return useContext(SocketContext);
+};
+export const SocketContextProvider = ({ children }) => {
+  const [socket, setSocket] = useState(null);
+  const { authUser } = useAuthContext();
+  const [onlineUsers, setonlineUsers] = useState([]);
+  // Effect for setting up the socket connection
+  useEffect(() => {
+    if (authUser) {
+      const socket = io("https://mern-chatapp1.onrender.com", {
+        query: {
+          userId: authUser._id,
+        },
+      });
+      setSocket(socket);
+
+      socket.on("getOnlineUsers", (users) => {
+        setonlineUsers(users);
+      });
+      return () => socket.close();
+    } else {
+      if (socket) {
+        socket.close();
+        setSocket(null);
+      }
+    }
+  }, [authUser]);
+  return (
+    <SocketContext.Provider value={{ socket, onlineUsers }}>
+      {children}
+    </SocketContext.Provider>
+  );
+};
